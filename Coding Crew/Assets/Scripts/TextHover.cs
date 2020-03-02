3ff592b767636deb0e4351dc65deb2d608d24327
Luke <lukewoodland2003@gmail.com>
2020-03-02 13:34:16 -0800

    Start Menu

---
+++ Coding Crew/Assets/Scripts/TextHover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextHover : MonoBehaviour
+{
+    public Text text;
+
+    public void OnMouseEnter()
+    {
+        text.color = Color.red;
+    }
+
+    public void OnMouseExit()
+    {
+        text.color = Color.white; 
+    }
+}
