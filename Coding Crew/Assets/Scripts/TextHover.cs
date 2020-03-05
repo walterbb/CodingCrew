a62ac93edf57e1e08499b02734cda3784fc5fd1b
Luke <lukewoodland2003@gmail.com>
2020-03-05 13:28:56 -0800

    Text Highlighting

--- Coding Crew/Assets/Scripts/TextHover.cs
+++ Coding Crew/Assets/Scripts/TextHover.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class TextHover : MonoBehaviour
+public class TextHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Text text;
 
-    public void OnMouseEnter()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        text.color = Color.red;
+        text.color = new Color(0,1f,1f,.9f);
+        text.fontSize = text.fontSize + 10;
     }
 
-    public void OnMouseExit()
+    public void OnPointerExit(PointerEventData eventData)
     {
-        text.color = Color.white; 
+        text.color = Color.white;
+        text.fontSize = text.fontSize - 10;
+    }
+
+    public void ButtonClicked()
+    {
+        text.color = Color.white;
+        text.fontSize = text.fontSize - 10;
     }
 }
