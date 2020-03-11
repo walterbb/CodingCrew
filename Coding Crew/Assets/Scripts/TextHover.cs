129ff24c075498ee51bb512f1e61266ae7789657
Luke <lukewoodland2003@gmail.com>
2020-03-11 13:36:38 -0700

    Volume Slider

--- Coding Crew/Assets/Scripts/TextHover.cs
+++ Coding Crew/Assets/Scripts/TextHover.cs
@@ -10,19 +10,16 @@ public class TextHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        text.color = new Color(0,1f,1f,.9f);
-        text.fontSize = text.fontSize + 10;
+         text.fontSize = text.fontSize + 10;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        text.color = Color.white;
         text.fontSize = text.fontSize - 10;
     }
 
     public void ButtonClicked()
     {
-        text.color = Color.white;
         text.fontSize = text.fontSize - 10;
     }
 }
